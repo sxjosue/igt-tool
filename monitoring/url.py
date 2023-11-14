3b0b15bd4b5f42e657055323a10b75b90b15082b
sxjosue <133665545+sxjosue@users.noreply.github.com>
2023-11-14 17:58:36 -0400

    Criação de novas telas

--- monitoring/url.py
+++ monitoring/url.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Apresentacao', views.Apresentacao, name='Apresentacao'),
+    path('Monitoramento', views.Monitoramento, name='Monitoramento'),
 ]
--- monitoring/views.py
+++ monitoring/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def Apresentacao(request):
+    return render(request, 'Apresentacao.html')
+
+def Monitoramento(request):
+    return render(request, "monitoramento.html")
